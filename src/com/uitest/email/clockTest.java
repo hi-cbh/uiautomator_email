728be73054ff0cf46e2cb036a522b316883f4e4e
hi-cbh <hi_cbh@qq.com>
2016-08-24 14:40:52 +0800

    更新clock，尝试jenkins

--- src/com/uitest/email/clockTest.java
+++ src/com/uitest/email/clockTest.java
@@ -1,5 +1,7 @@
 package com.uitest.email;
 
+import java.io.IOException;
+
 import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.Configurator;
 import com.android.uiautomator.core.UiObjectNotFoundException;
@@ -14,7 +16,7 @@ public class clockTest extends EmailTestBase {
 		String jarName = "clockTest";
 		String testClass = "com.uitest.email.clockTest";
 		String testName = "";
-		String androidId = "4";
+		String androidId = "2";
 		new UiAutomatorHelper(jarName, testClass, testName, androidId);
 	}
 	
@@ -29,39 +31,49 @@ public class clockTest extends EmailTestBase {
 	 * @throws UiObjectNotFoundException 
 	 * @throws java.text.ParseException 
 	 * @throws ParseException 
+	 * @throws IOException 
 	 */
-	public void testCase_001() throws RemoteException, UiObjectNotFoundException, ParseException, java.text.ParseException {
+	public void testCase_001() throws RemoteException, UiObjectNotFoundException, ParseException, java.text.ParseException, IOException {
 
-		long times = Configurator.getInstance().getActionAcknowledgmentTimeout();
-		Configurator.getInstance().setActionAcknowledgmentTimeout(0);
-		
-		long times2 = Configurator.getInstance().getWaitForSelectorTimeout();
-		Configurator.getInstance().setWaitForSelectorTimeout(0);
-		
-		clickById("com.sec.android.app.clockpackage:id/stopwatch_startButton");
-		
-		String start = getCurrentSysTime();
-		
-		UiDevice.getInstance().pressHome();
+		//Runtime.getRuntime().exec("am start com.sec.android.app.clockpackage/.ClockPackage");
 		
 		//sleep(5000);
 		
-		findAndOpenApp("时钟");
-
+//		long times = Configurator.getInstance().getActionAcknowledgmentTimeout();
+//		Configurator.getInstance().setActionAcknowledgmentTimeout(0);
+//		
+//		long times2 = Configurator.getInstance().getWaitForSelectorTimeout();
+//		Configurator.getInstance().setWaitForSelectorTimeout(0);
+		
+		//clickById("com.sec.android.app.clockpackage:id/stopwatch_startButton");
+		
+		//String start = getCurrentSysTime();
+		
+		UiDevice.getInstance().pressHome();
+		
+		sleep(2000);
+		
+		//findAndOpenApp("时钟");
+		Runtime.getRuntime().exec("am start com.sec.android.app.clockpackage/.ClockPackage");
+		
+		sleep(2000);
+		
+		UiDevice.getInstance().pressHome();
+		
 		//clickByName("秒表");
-		click(460, 120);
+		//click(460, 120);
 		
 		//sleep(1000);
 		
 		
 		//clickByName("停止");
-		click(180, 1200);
+		//click(180, 1200);
 		
-		String end = getCurrentSysTime();
-		
-		System.out.println("times: " + getTimeDistance(end, start));
-		Configurator.getInstance().setWaitForSelectorTimeout(times2);
-		Configurator.getInstance().setActionAcknowledgmentTimeout(times);
+//		String end = getCurrentSysTime();
+//		
+//		System.out.println("times: " + getTimeDistance(end, start));
+//		Configurator.getInstance().setWaitForSelectorTimeout(times2);
+//		Configurator.getInstance().setActionAcknowledgmentTimeout(times);
 	}
 	
 	
